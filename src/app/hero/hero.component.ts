f0d62afb8343a79685d828a1bbea9dcb49946088
Tu Vu <Tu.Vu@BlueKC.com>
2020-07-06 11:41:06 -0500

    Revert "Missed an isolated test"
    This reverts commit d5b977052bfad55b29c2d9b61bef00eec9114c3b.

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,14 +1,12 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Hero } from '../hero';
-import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls:  ['./hero.component.css']
 })
 export class HeroComponent {
-  // 4. Shallow Integration Tests
   @Input() hero: Hero;
   @Output() delete = new EventEmitter();
 
@@ -16,37 +14,4 @@ export class HeroComponent {
     $event.stopPropagation();
     this.delete.next();
   }
-
-  // 2-3. Isolated Tests
-  // heroes: Hero[];
-
-  // constructor(private heroService: HeroService) { }
-
-  // ngOnInit() {
-  //   this.getHeroes();
-  // }
-
-  // getHeroes(): void {
-  //   this.heroService.getHeroes()
-  //     .subscribe(heroes => this.heroes = heroes);
-  // }
-
-  // add(name: string): void {
-  //   name = name.trim();
-  //   var strength = 11
-  //   if (!name) {
-  //     return;
-  //   }
-  //   else {
-  //     this.heroService.addHero({ name, strength } as Hero)
-  //       .subscribe(hero => {
-  //         this.heroes.push(hero);
-  //       })
-  //   }
-  // }
-
-  // delete(hero: Hero): void {
-  //   this.heroes = this.heroes.filter(h => h !== hero);
-  //   this.heroService.deleteHero(hero).subscribe();
-  // }
 }
